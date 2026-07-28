fd15946c0f7a035c975cdde2b42ce2d354204e96
Sourajit-M <sourajitm19@gmail.com>
2026-07-29 01:35:13 +0530

    added streaming feature of LLM

---
+++ week_02/04_streaming.py
@@ -0,0 +1,50 @@
+#dont use stream while giving the response back to llm (json format)
+# reduces the waiting time
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+#ROLE:
+#TASK
+#CONSTRAINT
+#OUTPUT FORMAT
+#Example
+#FALLBACK
+
+API_KEY = os.getenv("GROQ_API_KEY")
+
+if not API_KEY:
+    raise ValueError("API KEY NOT FOUND!")
+
+client = Groq(api_key=API_KEY)
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role" : "system",
+            "content":"""
+            You are Engineering Professor assistant. 
+            Give answer to the specific question asked by the client.
+            Do not invent answer or give vague response
+            make the answer more readable 
+            If you dont know the answer say "Ask different question"
+            """
+        },
+        {
+            "role": "user",
+            "content": "How does internet work?",
+        }
+    ],
+    stream=True
+)
+
+
+for chunk in response:
+    content = chunk.choices[0].delta.content
+    if content:
+        print(content, end="", flush=True)
